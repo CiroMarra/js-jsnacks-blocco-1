92224c2bacea6473a709fd956d100d788ca6d5ea
CiroMarra <tensha93@gmail.com>
2024-03-11 15:43:57 +0100

    feat:  add script to snacks-2

--- snacks-2/js/script.js
+++ snacks-2/js/script.js
@@ -1,2 +1,11 @@
 //Il software deve chiedere per 5 volte all’utente di inserire un numero.
 //Il programma stampa la somma di tutti i numeri inseriti.
+
+// chiede i numeri a l'utente fino a cinque volte.
+let numeriUtente = 0;
+for (let i = 0; i < 5; i++) {
+    // somma i numeri che l'utente ha inserito.
+numeriUtente += parseInt(prompt('Inserisci il numero '));
+}
+    // controlla la somma totale dei numeri inseriti dell'utente.
+    console.log(numeriUtente);
